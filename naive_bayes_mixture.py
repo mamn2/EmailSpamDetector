ef2d3ef816b532de74e105e11dba24900ebc1a8c
Mohamed Amn <mamn2@illinois.edu>
2021-02-24 16:52:50 -0600

    mixture implemented

--- naive_bayes_mixture.py
+++ naive_bayes_mixture.py
@@ -14,8 +14,65 @@ within this file for Part 2 -- the unrevised staff files will be used for all ot
 files and classes when code is run, so be careful to not modify anything else.
 """
 
+import math
 
+def trainBigramClassifier(train_set, train_labels, laplace):
 
+    bigrams = {}
+
+    # first value is for num words in spam mails, second is for num words in non-spam
+    numBigramsPerClass = [0, 0]
+
+    for i in range(len(train_labels)):
+        for j in range(len(train_set[i]) - 1):
+            bigram = (train_set[i][j], train_set[i][j + 1])
+            if bigram in bigrams:
+                bigrams[bigram][train_labels[i]] += 1
+                numBigramsPerClass[train_labels[i]] += 1
+            else:
+                if train_labels[i] == 0:
+                    bigrams[bigram] = [1, 0]
+                else:
+                    bigrams[bigram] = [0, 1]
+          
+    probBigramGivenClass = {}
+
+    uniqueBigrams = len(bigrams)
+
+    for bigram in bigrams:
+        probBigramGivenClass[(bigram, 0)] = math.log((bigrams[bigram][0] + laplace) / (numBigramsPerClass[0] + uniqueBigrams * laplace))
+        probBigramGivenClass[(bigram, 1)] = math.log((bigrams[bigram][1] + laplace) / (numBigramsPerClass[1] + uniqueBigrams * laplace))
+
+    return (probBigramGivenClass, uniqueBigrams, numBigramsPerClass)
+   
+def trainUnigramClassifier(train_set, train_labels, laplace):
+
+    words = {}
+
+    # first value is for num words in spam mails, second is for num words in non-spam
+    numWordsPerClass = [0, 0]
+
+    for i in range(0, len(train_labels)):
+        for word in train_set[i]:
+            if word in words:
+                words[word][train_labels[i]] += 1
+                numWordsPerClass[train_labels[i]] += 1
+            else:
+                if train_labels[i] == 0:
+                    words[word] = [1, 0]
+                else:
+                    words[word] = [0, 1]
+          
+    probWordGivenClass = {}
+
+    uniqueWords = len(words)
+
+    for word in words:
+        probWordGivenClass[(word, 0)] = math.log((words[word][0] + laplace) / (numWordsPerClass[0] + uniqueWords * laplace))
+        probWordGivenClass[(word, 1)] = math.log((words[word][1] + laplace) / (numWordsPerClass[1] + uniqueWords * laplace))
+
+    return (probWordGivenClass, uniqueWords, numWordsPerClass)
+    
 
 def naiveBayesMixture(train_set, train_labels, dev_set, bigram_lambda,unigram_smoothing_parameter, bigram_smoothing_parameter, pos_prior):
     """
@@ -42,6 +99,52 @@ def naiveBayesMixture(train_set, train_labels, dev_set, bigram_lambda,unigram_sm
     # TODO: Write your code here
     # return predicted labels of development set
 
+    dev_labels = []
 
+    probWordGivenClass, uniqueWords, numWordsPerClass = trainUnigramClassifier(train_set, train_labels, unigram_smoothing_parameter)
+    probBigramGivenClass, uniqueBigrams, numBigramsPerClass = trainBigramClassifier(train_set, train_labels, bigram_smoothing_parameter)
 
-    return []
+    for doc in dev_set:
+        probHamGivenWord = math.log(pos_prior)
+        probSpamGivenWord = math.log(1 - pos_prior)
+        probHamGivenBigram = math.log(pos_prior)
+        probSpamGivenBigram = math.log(1 - pos_prior)
+        for i in range(len(doc)):
+
+            #unigram
+            word = doc[i]
+            if (word, 0) not in probWordGivenClass:
+                continue
+            if probWordGivenClass[(word, 0)] == 0:
+                probSpamGivenWord += math.log(unigram_smoothing_parameter / (numWordsPerClass[0] + uniqueWords * unigram_smoothing_parameter))
+            else:
+                probSpamGivenWord += probWordGivenClass[(word, 0)]
+            if probWordGivenClass[(word, 1)] == 0:
+                probHamGivenWord += math.log(unigram_smoothing_parameter / (numWordsPerClass[1] + uniqueWords * unigram_smoothing_parameter))
+            else:
+                probHamGivenWord += probWordGivenClass[(word, 1)]
+
+            #bigram
+            if i == len(doc) - 1:
+                continue
+            bigram = (doc[i], doc[i + 1])
+            if (bigram, 0) not in probBigramGivenClass:
+                continue
+            if probBigramGivenClass[(bigram, 0)] == 0:
+                probSpamGivenBigram += math.log(bigram_smoothing_parameter / (numBigramsPerClass[0] + uniqueWords * bigram_smoothing_parameter))
+            else:
+                probSpamGivenBigram += probBigramGivenClass[(bigram, 0)]
+            if probBigramGivenClass[(bigram, 1)] == 0:
+                probHamGivenBigram += math.log(bigram_smoothing_parameter / (numBigramsPerClass[1] + uniqueWords * bigram_smoothing_parameter))
+            else:
+                probHamGivenBigram += probBigramGivenClass[(bigram, 1)]
+
+        probSpam = (((1 - bigram_lambda) * probSpamGivenWord) + (bigram_lambda * probSpamGivenBigram))
+        probHam = (((1 - bigram_lambda) * probHamGivenWord) + (bigram_lambda * probHamGivenBigram))
+
+        if probHam >= probSpam:
+            dev_labels.append(1)
+        else:
+            dev_labels.append(0)
+
+    return dev_labels
